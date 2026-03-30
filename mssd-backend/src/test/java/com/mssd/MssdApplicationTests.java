b95765199111a3e1c9d9cda5ec68e012399504a5
souhail <souhailabdennebi2@gmail.com>
2026-03-30 11:39:43 +0100

    CI/CD

--- mssd-backend/src/test/java/com/mssd/MssdApplicationTests.java
+++ mssd-backend/src/test/java/com/mssd/MssdApplicationTests.java
@@ -2,8 +2,10 @@ package com.mssd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class MssdApplicationTests {
 
     @Test
